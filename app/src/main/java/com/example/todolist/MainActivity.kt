523716f9a04fb1a265726b3279c89046a8ef4eaf
Allan Jackson <allan.oliveira@smartsw.com.br>
2021-09-11 23:02:03 -0300

    MainActivity XML layout completed
    Some logic was put into MainActivity.kt and the XML was finalized

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,10 +2,64 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todolist.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mRecycler: RecyclerView
+    private lateinit var mCurrentLayoutManager: LayoutType
+
+    private enum class LayoutType{
+        LINEAR_LAYOUT,
+        GRID_LAYOUT
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setuping view binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //set the mRecycler variable and handle recyclerView configurations
+        setupRecyclerView()
+
+
+        if(savedInstanceState == null){
+            mCurrentLayoutManager = LayoutType.LINEAR_LAYOUT
+            setupListeners()
+        }
+
+        setListLayout(mCurrentLayoutManager)
     }
+
+    private fun setupListeners() {
+        binding.imgBtnListView.setOnClickListener(this)
+        binding.imgBtnGridView.setOnClickListener(this)
+    }
+
+    private fun setupRecyclerView() {
+        mRecycler = binding.rcylerViewNoteList
+    }
+
+    private fun setListLayout(layoutType: LayoutType) {
+        mCurrentLayoutManager = layoutType
+        mRecycler.layoutManager = when(mCurrentLayoutManager){
+            LayoutType.LINEAR_LAYOUT -> LinearLayoutManager(this)
+            LayoutType.GRID_LAYOUT -> GridLayoutManager(this,2)
+        }
+    }
+
+    override fun onClick(view: View) {
+        when(view.id){
+            binding.imgBtnGridView.id -> setListLayout(LayoutType.GRID_LAYOUT)
+            binding.imgBtnListView.id -> setListLayout(LayoutType.LINEAR_LAYOUT)
+        }
+    }
+
+
 }
